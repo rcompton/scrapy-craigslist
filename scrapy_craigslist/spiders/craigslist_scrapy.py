0207695e2b97e288bca68628271e02ca4954c2f9
rcompton <ryan@clarifai.com>
2016-01-03 00:08:26 -0500

    refactor a bit, don't follow every link and allow arguments to spider

--- scrapy_craigslist/spiders/craigslist_scrapy.py
+++ scrapy_craigslist/spiders/craigslist_scrapy.py
@@ -1,13 +1,14 @@
 __author__ = 'htm'
 
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
-from scrapy.contrib.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
 from scrapy.selector import Selector
 from scrapy_craigslist.items import ScrapyCraigslistItem
 
+DOWNLOAD_DELAY = 0.5    # craig blocks you fast
 
-class MySpider(CrawlSpider):
+class CraigslistSpider(CrawlSpider):
     """
     This CrawlSpider will look into the specific city and pull out content for
     Title, Ad's URL, Post Date, Post Date Specific (i.e. datetime), Price,
@@ -21,34 +22,27 @@ class MySpider(CrawlSpider):
 
     """
     name = 'craigslist'
-    allowed_domains = ['sfbay.craigslist.org']
-    start_urls = ['http://sfbay.craigslist.org/search/apa?']
 
-    # rules = (
-    #     # Scrape all pages of results, not just the first page.
-    #     Rule(LinkExtractor(
-    #         allow = (r'.*/search/apa\?s\=\d+.*'),
-    #         deny = (r'.*format\=rss.*')
-    #     ), callback='parse_items_1', follow=True),
-    #
-    #     # Extract all data from each results page.
-    #     Rule(LinkExtractor(allow=(r'.*/apa/.*\.html$')), callback='parse_items_1'),
-    # )
+    def __init__(self, city='sfbay', topic='rid'):
+        self.allowed_domains = ['{}.craigslist.org'.format(city)]
+        self.topic = topic
+        self.city = city
+        self.start_urls = ['http://{0}.craigslist.org/search/{1}?'.format(city, topic)]
+        # self.rules = (
+        #     Rule(LxmlLinkExtractor(
+        #         allow=(r'{0}.craigslist.org/.*/{1}/.*'.format(city, topic)),
+        #         # allow=(r'.*/search/apa\?s\=\d+.*'),
+        #         deny = (r'.*format\=rss.*')
+        #     ),
+        #         callback="parse_items_1",
+        #         follow= False,
+        #          ),
+        #     )
+        super(CraigslistSpider, self).__init__()
 
-    rules = (
-        Rule(LxmlLinkExtractor(
-            allow=(r'sfbay.craigslist.org/.*'),
-            # allow=(r'.*/search/apa\?s\=\d+.*'),
-            deny = (r'.*format\=rss.*')
-        ),
-            callback="parse_items_1",
-            follow= True,
-             ),
-        # Rule(LxmlLinkExtractor(allow=("search/apa?s=d00&")), callback="parse_items_2", follow= True),
-        )
-
-
-# allow=(r'sfbay.craigslist.org/search')
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url, callback=self.parse_items_1)
 
     def parse_items_1(self, response):
         """
@@ -60,13 +54,14 @@ class MySpider(CrawlSpider):
         """
         items = []
         hxs = Selector(response)
-        # print response.url
+        #import pdb; pdb.set_trace()
+        print response.url
         contents = hxs.xpath("//div[@class='content']/*")
         for content in contents:
             item = ScrapyCraigslistItem()
             item ["title"] = content.xpath("//p/span/span/a/text()").extract()[0]
             k = content.xpath("//p/a/@href").extract()[0]
-            item ['ad_url'] = 'http://sfbay.craigslist.org{}'.format(''.join(k))
+            item ['ad_url'] = 'http://{0}.craigslist.org{1}'.format(self.city, ''.join(k))
             # item ["img_url"] = content.select("(//img//@src)").extract() # BAAAD
             item ["post_date"] = content.xpath("//p/span/span/time/text()").extract()[0]
             item ["post_date_specific"] = content.xpath("//p/span/span/time/@datetime").extract()[0]
